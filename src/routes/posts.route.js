ec3e93fafe0f6c84e45bd91721a88285e6223197
BraulioNR <branoleruiz@gmail.com>
2022-05-16 03:31:42 -0500

    Update route of posts

--- src/routes/posts.route.js
+++ src/routes/posts.route.js
@@ -12,11 +12,12 @@ const { isAuth } = require("../utils/auth")
 
 /* Creating a route for the `/` path, and then adding a `post` and `get` method to it using isAuthenticated. */
 router.route("/").post(isAuth, create).get(listAll)
+router.route("/mylist").get(isAuth, mylist)
 /* Creating a route for the `/:id` path, and then adding a `get`, `delete`, and `put` method to it using isAuthenticated. */
 router
   .route("/:id")
   .get(isAuth, show)
   .delete(isAuth, destroy)
   .put(isAuth, update)
-router.route("/mylist").get(isAuth, mylist)
+
 module.exports = router
